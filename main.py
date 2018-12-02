e1daf7ca6c31f22af00697318a50e50b8127acd0
weilin0406 <45517857+weilin0406@users.noreply.github.com>
2018-12-02 15:40:43 -0500

    Add files via upload
    Permission denied Error 403 while pushing through GIT

---
+++ main.py
@@ -0,0 +1,55 @@
+from flask import Flask, request
+from caesar import rotate_string
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+
+form = """
+<!DOCTYPE html>
+<html>
+    <head>
+    
+        <style>
+            form {{
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }}
+            textarea {{
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }}
+           
+        </style>
+    </head>
+    <body>
+        <form action="/new" method="post">
+            <label>Rotate by:</label>
+            <input type="text" id="rot" name="rot" value="0" />
+            <textarea name="text">{0}</textarea>
+            <input type="submit" />
+        </form>
+    </body>
+</html>
+"""
+@app.route("/new", methods=['POST'])
+def encrypt():
+    rot = int(request.form["rot"])
+    text = request.form["text"]
+    cat = rotate_string(text, rot)
+
+
+    return form.format(cat)
+
+
+
+@app.route("/")
+def index():
+    return form.format("")
+
+app.run()
